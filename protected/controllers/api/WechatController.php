4d1b6d4971365d1eada3e775823d3a4cd5642405
n3yang <n3yang@gmai.com>
2013-10-06 23:44:04 +0800

    update

--- protected/controllers/api/WechatController.php
+++ protected/controllers/api/WechatController.php
@@ -77,20 +77,18 @@ class WechatController extends Controller
 		$s = new SpiderSina;
 		$stock = $s->getStock($sid);
 		$message = "名  称：".$stock['name']."\n"
-			.'涨  跌：'.$stock['open']."\n"
-			.'当  前：'.$stock['current']."\n"
-			.'今  开：'.$stock['open']."\n"
-			.'最  高：'.$stock['max']."\n"
-			.'最  低：'.$stock['min']."\n"
-			.'昨  收：'.$stock['yestoday']."\n"
-			.'成交量：'.$stock['turnover']."\n"
-			.'成交额：'.$stock['turnover_v']."\n"
-			.'总市值：'.$stock['total_v']."\n"
-			.'振  幅：'.$stock['swing']."\n"
-			.'换手率：'.$stock['exchange']."\n"
-			.'市净率：'.$stock['pb']."\n"
-			.'市盈率：'.$stock['ttm']."\n"
-			.'更新时间：'.date('Y-m-d H:i:s', strtotime($stock['update']));
+			.'上涨：'.$stock['current']-$stock['open'].'（'.($stock['current']-$stock['open'])/$stock['open']."）\n"
+			.'当前：'.$stock['current']."\n"
+			.'今开：'.$stock['open']."  ".'昨收：'.$stock['yestoday']."\n"
+			.'最高：'.$stock['max']."  ".'最低：'.$stock['min']."\n"
+			.'成交量：'.$stock['turnover']."万手\n"
+			.'成交额：'.$stock['turnover_v']."万元\n"
+			.'总市值：'.$stock['total_v']."亿元\n"
+			.'振  幅：'.$stock['swing']."%\n"
+			.'换手率：'.$stock['exchange']."%\n"
+			.'市净率：'.$stock['pb']."%\n"
+			.'市盈率：'.$stock['ttm']."%\n"
+			.'更新时间：'.date('m-d H:i:s', $stock['update']);
 		$this->oWechat->text($message)->reply();
 	}
 
